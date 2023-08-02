681e612bbac52dc385d7bd7cc612b445cdbbf1f7
Azad Zeynalov <azeynalov1@mtrx-node04pd.dcm.senecacollege.ca>
2023-08-02 11:42:54 -0400

    Last Library Reference Number incremented in save method

--- FinalProject/ms5test/LibApp.cpp
+++ FinalProject/ms5test/LibApp.cpp
@@ -65,6 +65,7 @@ namespace sdds {
 				if (m_PPA[i]->getRef() != 0) {
 					if (m_PPA[i]->getRef() == -1) {
 						m_PPA[i]->setRef(m_LLRN);
+						m_LLNR++;
 					}
 					outFile << *m_PPA[i] << endl;
 				}
